2c25edb1140345ef9d174bd44efae2cb42c0ebe5
kimseunghyun-kr <incongnito12@gmail.com>
2023-08-04 17:22:36 +0800

    main layout for controller and service done. content is to be added after resolving API

---
+++ src/main/java/com/stock/oppenheimer/controller/RegisteredTickerController.java
@@ -0,0 +1,11 @@
+package com.stock.oppenheimer.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+@RequestMapping("/registered")
+public class RegisteredTickerController {
+}
