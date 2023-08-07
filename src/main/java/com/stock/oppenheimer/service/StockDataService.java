2fdc6873d2b5cb20696832ffaeb53ec170f1b7ba
kimseunghyun-kr <incongnito12@gmail.com>
2023-08-07 15:06:01 +0800

    base level changes

---
+++ src/main/java/com/stock/oppenheimer/service/StockDataService.java
@@ -0,0 +1,16 @@
+package com.stock.oppenheimer.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class StockDataService {
+    public List<String> findAllAvailableTickers() {
+        return
+    }
+}
