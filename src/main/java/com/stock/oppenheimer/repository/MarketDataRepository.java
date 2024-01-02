1279c95a8510578d991e3cd458dbb1b090e9b63a
kimseunghyun-kr <incongnito12@gmail.com>
2024-01-02 18:02:50 +0900

    research for volume profile indicator. to create a CPP module for volume profile indicator to be linked to main program

--- src/main/java/com/stock/oppenheimer/repository/MarketDataRepository.java
+++ src/main/java/com/stock/oppenheimer/repository/MarketDataRepository.java
@@ -2,9 +2,26 @@ package com.stock.oppenheimer.repository;
 
 import com.stock.oppenheimer.domain.MarketData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public interface MarketDataRepository extends JpaRepository<MarketData, Long> {
     List<MarketData> findAllByStockDataStockName(String stockName);
+
+    @Query("SELECT m.close, SUM(m.volume) FROM MarketData m WHERE m.stockData.stockName = :stockName GROUP BY m.close")
+    List<Object[]> findCloseAndSumVolumeByStockName(@Param("stockData") String stockName);
+
+    default Map<Long, Long> findCloseAndSumVolumeMapByStockData(String stockName) {
+        List<Object[]> result = findCloseAndSumVolumeByStockName(stockName);
+
+        return result.stream()
+                .collect(Collectors.toMap(
+                        array -> (Long) array[0],  // close
+                        array -> (Long) array[1]   // sum of volume
+                ));
+    }
 }
